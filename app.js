893f9740351a4f5454e6fa6fafe936d17ca046fe
masaki-oomura <masaki.oomura.vy@gmail.com>
2023-07-26 13:56:04 +0900

    fix port number

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const os = require('os');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
