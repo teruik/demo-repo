a25b6d2fcd73d5adf20fb14357ea332884bab946
masaki-oomura <masaki.oomura.vy@gmail.com>
2023-07-28 10:27:56 +0900

    no message

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-const http = require('http');
-const os = require('os');
+const http = require('http'); 
+const os = require('os'); //Node.jsのデフォルト機能でOS情報を取得してます
 const port = 8080;
-/* //改修するときには左端のコメントアウトを削除
-const app = express();
-const express = require('express');
-*/ //改修するときには左端のコメントアウトを削除 
 
-
-// /* 改修するときには左端のコメントアウトを削除
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'application/json');
   
-    const hostname = os.hostname();
+    const hostname = os.hostname(); // OS情報は色々あるのですが、その中からホスト名だけを取ってきています
   
-    response.write(JSON.stringify(hostname));
+    response.write(JSON.stringify(hostname)); // ホスト名をJSON文字列で返します
     response.end();
   });
 
-  server.listen(8080, () => {
-    console.log(`Server running on port ${port}`);
+  server.listen(port, () => {
+    console.log(`Server running on port ${port}`); //サーバを立ち上げたときにコンソールに出すメッセージです
   });
   
-//   改修するときには左端のコメントアウトを削除 */ 
-
-/* //改修するときには左端のコメントアウトを削除
-app.set('view engine', 'ejs');
-app.set('views', './views');
-
-app.get('/', (req, res) => {
-  const hostname = os.hostname();
-  res.render('index', { hostname });
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-*/ //改修するときには左端のコメントアウトを削除 
