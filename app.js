bc6bc3d69ea21488d91cf17b90f8c65394744294
masaki-oomura <masaki.oomura.vy@gmail.com>
2023-07-28 10:36:58 +0900

    no message

--- app.js
+++ app.js
@@ -1,32 +1,12 @@
 const http = require('http');
 const os = require('os');
 const port = 8080;
-//改修するときには左端のコメントアウトを削除
-const express = require('express');
-const app = express();
- //改修するときには左端のコメントアウトを削除 
+const express = require('express'); // HTMLを記述したファイルを扱うためのおまじない
+const app = express(); // 上に同じ
 
 
- /* 改修するときには左端のコメントアウトを削除
-const server = http.createServer((request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-  
-    const hostname = os.hostname();
-  
-    response.write(JSON.stringify(hostname));
-    response.end();
-  });
-
-  server.listen(8080, () => {
-    console.log(`Server running on port ${port}`);
-  });
-  
-   改修するときには左端のコメントアウトを削除 */ 
-
- //改修するときには左端のコメントアウトを削除
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', './views'); // viewsディレクトリ内の.ejsファイルを読み込む為のおまじない
 
 app.get('/', (req, res) => {
   const hostname = os.hostname();
@@ -36,4 +16,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
- //改修するときには左端のコメントアウトを削除 
